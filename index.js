99bd46b8c52736ca45ebd5d255d41c7d7e9a80d6
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-05-12 17:51:07 +0600

    half

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
+
 // const rootCall = (req, res) => res.send('Thank you very much')
 
-app.get('/', (req, res) => {
-    res.send("I know how to open Node....YAY") 
-});
+// app.get('/', (req, res) => {
+//     res.send("Node Ami onek pari....YAY") 
+// });
+
+// app.get('/', (req, res) => {
+
+//     const fruit = {
+//         product: 'Mango',
+//         price: 70
+//     }
+//     res.send(fruit) 
+// });
+
+// app.get('/fruits/banana', (req, res)=> {
+//     res.send({fruit: 'banana', quantity: 1000, price: 10000})
+// })
+
+const users = [ "korim", 'Moin', 'sabed', 'Susmita', 'Sohana', 'sabana' ]
+
+app.get('/users/:id', (req, res)=> {
+    const userId = (req.params.id)
+    console.log(req.query.sort)
+    const name = users[userId]
+    res.send({userId, name});
+})
 
 app.listen(4000, ()=> console.log('Listening to port 4000'));
