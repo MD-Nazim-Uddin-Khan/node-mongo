6ac63d7cc3c120f92e65a60bc5825f26b6a9fe55
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-05-12 15:28:07 +0600

    my first node project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+
+const app = express();
+
+// const rootCall = (req, res) => res.send('Thank you very much')
+
+app.get('/', (req, res) => {
+    res.send("I know how to open Node....YAY") 
+});
+
+app.listen(4000, ()=> console.log('Listening to port 4000'));
